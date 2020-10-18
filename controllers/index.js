1b83cb459c3f8d9069c82af417ddeaaa7da03269
liuyi <794516454@qq.com>
2020-10-18 16:54:45 +0800

    增加静态文件

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,7 @@ const {
 } = require("vm")
 
 var fn_index = async (ctx, next) => {
-  ctx.response.body = `<h1>Index</h1>
-  <form action="/signin" method="post">
-  <p>Name:<input  name="name" value="koa"></p>
-  <p>Password:<input name="password" type="password"></p>
-  <p><input type="submit" value="Submit"></p>
-  </form>
-  `
-
+  ctx.render('index.html')
 }
 
 
